c21be05168977d29e86a611d6d78a8433fef556c
Pavel Tolmachev <betadecay1993@gmail.com>
2026-02-13 16:16:58 -0500

    Fixed a bug in configs: instead of tuple for bias_range I should have used a list

--- slurm/submit_all.sh
+++ slurm/submit_all.sh
@@ -2,11 +2,16 @@
 set -e -o pipefail
 shopt -s nullglob
 
-FILES=( *.slurm )
+EXCLUDE=(BareTraining.slurm SparsityOnly.slurm FRMagnitudeOnlyOnly.slurm BareTrainingMemoryAntiAngle.slurm SparsityAndFRMagnitudeMemoryAntiAngle.slurm SparsityAndFRMagnitude.slurm SparsityAndFRMagnitudeAlphaBetaSweep.slurm Dropout.slurm)  # <-- add files here (exact names)
 
+declare -A SKIP
+for x in "${EXCLUDE[@]}"; do SKIP["$x"]=1; done
+
+FILES=( *.slurm )
 ((${#FILES[@]})) || { echo "No .slurm files found."; exit 1; }
 
 for f in "${FILES[@]}"; do
+  [[ ${SKIP["$f"]+x} ]] && { echo "Skipping $f"; continue; }
   echo "Submitting $f"
   sbatch "$f"
 done
